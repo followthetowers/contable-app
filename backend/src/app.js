efd7ac031bd7c467f3a64c1436b238b870a83400
Chris Towers <towersi@MacBook-Pro-de-Chris-2.local>
2026-06-02 00:56:01 -0300

    Simplificar CORS para permitir todos los origenes

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,7 +4,7 @@ const seed    = require('./db/seed');
 
 const app = express();
 
-app.use(cors({ origin: process.env.FRONTEND_URL || 'http://localhost:5173', credentials: true }));
+app.use(cors());
 app.use(express.json());
 
 // Rutas
